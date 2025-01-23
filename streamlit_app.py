05162a6f2a89b4a16c36a9ac3df1f8786fe64034
chloe <149999780+tksy1227@users.noreply.github.com>
2025-01-23 13:07:30 +0000

    2nd commit YAHOOOOO

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,17 @@
 import pandas as pd 
 import streamlit as st
 
+data = pd.read_csv('C:\Users\User\OneDrive - Ngee Ann Polytechnic\Ngee Ann Poly\YEAR 2 SEM 2\DDP\ASSG2\ddp_data.csv')
+
+# Create Streamlit UI
+st.title("Ngee Ann Polytechnic Bus Arrival Information")
+bus_stop_code = st.text_input("Enter Bus Stop Code (12101/12109):")
+
+# Filter data
+if bus_stop_code:
+    filtered_data = data[data['BusStopCode'] == bus_stop_code]
+else:
+    filtered_data = data
+
+# Display data
+st.dataframe(filtered_data)
